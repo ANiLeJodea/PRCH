a67c52768692fec64c31f6ea8399173176b048d0
DandyDrop <94701539+DandyDrop@users.noreply.github.com>
2023-05-24 18:52:39 +0300

    Rename variables and functions (and add comments) ; Add more log potential to verify_proxy_on_ipinfo function ; fix setup.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+# Pre-installed packages
 import os
 import threading
 import time
 
+# External packages
 import requests
 import telebot
 from flask import Flask, request
@@ -10,6 +12,7 @@ LOG_FORUM_ID, LOG_TOPIC_ID = os.environ['LOG_ENTITY'].split(' ')
 app = Flask(__name__)
 bot = telebot.TeleBot(token=os.environ["BOT_TOKEN"])
 ADMIN_IDS = [652015662, 1309387740]
+main_command = "c"
 
 @app.route('/', methods=["POST"])
 def handle_request():
@@ -25,25 +28,28 @@ def handle_request():
 def say(m):
     bot.send_message(m.chat.id, "The bot is ready to perform\nSend command /c followed by the pr u want to check")
 
-@bot.message_handler(commands=['c'])
+@bot.message_handler(commands=[main_command])
 def run_and_answer(m):
-    msg_id = bot.send_message(m.chat.id, "Trying to verify...").id
-    thread = threading.Thread(target=perform, args=(m.chat.id, msg_id, m.text[3:]))
+    answer_message_id = bot.send_message(m.chat.id, "Trying to verify...").id
+    thread = threading.Thread(
+        target=perform, args=(m.chat.id, answer_message_id, m.text[len(main_command)+2:])
+    )
     thread.start()
 
-def perform(chat_id, id_of_message_to_change, pripp):
-    pripp_list = pripp.split(':')
-    if len(pripp_list) == 2:
-        text = verify_pr_on_ipinfo(*pripp_list)
+def perform(chat_id, id_of_message_to_change, proxy_ip_port):
+    proxy_ip_port_list = proxy_ip_port.split(':')
+    if len(proxy_ip_port_list) == 2:
+        text = verify_proxy_on_ipinfo(*proxy_ip_port_list)
     else:
         text = "Please, provide something to check in the correct format."
+
     bot.edit_message_text(
         text=text,
         chat_id=chat_id,
         message_id=id_of_message_to_change
     )
 
-def verify_pr_on_ipinfo(
+def verify_proxy_on_ipinfo(
         proxy_ip: str, proxy_port: str, timeout: int = 5
 ) -> str:
     try:
@@ -55,14 +61,33 @@ def verify_pr_on_ipinfo(
         time_taken = round(time.time() - t, 4)
         if r.status_code == 200:
             if r.text.strip() == proxy_ip:
-                return f"Time taken: {time_taken}"
+                return f"The proxy worked.\nr.text: {r.text}\nTime taken: {time_taken}"
             else:
-                return "IpInfo did not show the ip of the proxy. " \
-                       f"Seems like a proxy is not working.\nTime taken: {time_taken}"
+                return "Seems like IpInfo didnt show the ip of the proxy properly." \
+                       f"\nr.text: {r.text}\nTime taken: {time_taken}"
 
     except Exception as e:
-        return f"Got the exception:\n{e}\nClass: {e.__class__}"
+        return f"Got the exception:\n{e}\n\nException class: {e.__class__}"
 
+
+def verify_proxy_on_site_list(
+        proxy_ip: str, proxy_port: str, site_list: list, timeout: int = 5
+) -> dict:
+    test_results = {}
+    proxy_ip_port = f"{proxy_ip}:{proxy_port}"
+    for site in site_list:
+        try:
+            t = time.time()
+            r = requests.get(site, proxies={"http": proxy_ip_port, "https": proxy_ip_port}, timeout=timeout)
+            time_taken = round(time.time() - t, 4)
+            if r.status_code == 200:
+                test_results[site] = (True, time_taken)
+        except Exception as e:
+            test_results[site] = (False, e)
+        time.sleep(3)
+
+
+    return test_results
 
 
 if __name__ == "__main__":
--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ import requests
 
 LOG_FORUM_ID, LOG_TOPIC_ID = os.environ['LOG_ENTITY'].split(' ')
 
-response = requests.get(f"https://api.telegram.org/bot{os.environ['BOT_TOKEN']}/sendMessage", {
+requests.get(f"https://api.telegram.org/bot{os.environ['BOT_TOKEN']}/sendMessage", {
     'chat_id': LOG_FORUM_ID,
-    'top_msg_id': LOG_TOPIC_ID,
-    'text': requests.get(f"https://api.telegram.org/bot{os.environ['BOT_TOKEN']}/setwebhook?url="
-                         f"{os.environ['EXTERNAL_URL']}").text
+    'message_thread_id': LOG_TOPIC_ID,
+    'text': "Tried to set a webhook. Response:\n" +
+            requests.get(f"https://api.telegram.org/bot{os.environ['BOT_TOKEN']}/setwebhook?url="
+                         f"{os.environ['EXTERNAL_URL']}").json()['description']
 })
-
