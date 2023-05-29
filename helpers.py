e6066c84f1f6791a0a4db0a213ded2fdf1e06037
DandyDrop <94701539+DandyDrop@users.noreply.github.com>
2023-05-29 21:58:38 +0300

    Add number_to_show parameter to prettify in helpers.py

--- helpers.py
+++ helpers.py
@@ -12,16 +12,16 @@ def exc_to_str(
         type(exc), exc, exc.__traceback__,
         limit=limit, chain=chain))
 
-def prettify(arg, dec_start: str = '<strong>', dec_end: str = '</strong>'):
+def prettify(arg, dec_start: str = '<strong>', dec_end: str = '</strong>', number_to_show: int = 3):
     t = type(arg)
     if t in [list, tuple]:
-        return f'''\n{str(t).split("'")[1]} : ''' + ' ; '.join(
+        return f'''{number_to_show} first:\n{str(t).split("'")[1]} : ''' + ' ; '.join(
             f'''{dec_start}{a}{dec_end} ({str(type(a)).split("'")[1]})'''
-            for a in arg[:3])
+            for a in arg[:number_to_show])
     elif t == dict:
-        return f'''{str(t).split("'")[1]} : ''' + ' ; '.join(
+        return f'''{number_to_show} first:\n{str(t).split("'")[1]} : ''' + ' ; '.join(
             f"""{dec_start}{k}{dec_end} : {dec_start}{v}{dec_end} ({str(type(v)).split("'")[1]})"""
-            for k, v in arg.items()[:3])
+            for k, v in arg.items()[:number_to_show])
     else:
         return f"{dec_start}{arg}{dec_end}"
 
