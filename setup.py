87a96de49b991c0c5c097ba986b463110514a2e5
ANiLeJodea <125437988+ANiLeJodea@users.noreply.github.com>
2023-06-20 15:12:47 +0300

    Make time.sleep() dynamic in EncTeleBot

--- setup.py
+++ setup.py
@@ -19,7 +19,8 @@ class EncTeleBot(TeleBot):
             disable_web_page_preview: bool = True,
             reply_to_message_id: int = None,
             timeout: int = None,
-            message_thread_id: int | str = None
+            message_thread_id: int | str = None,
+            delay_between_sending: float = 2
     ) -> list[types.Message]:
         to_return = []
         for t in util.smart_split(text=text):
@@ -27,7 +28,7 @@ class EncTeleBot(TeleBot):
                 chat_id=chat_id, text=t, parse_mode=parse_mode, disable_web_page_preview=disable_web_page_preview,
                 reply_to_message_id=reply_to_message_id, timeout=timeout, message_thread_id=message_thread_id
             ))
-            time.sleep(2)
+            time.sleep(delay_between_sending)
         return to_return
 
     def enc_edit_message_text(
@@ -36,7 +37,8 @@ class EncTeleBot(TeleBot):
             parse_mode: str = None, entities: list[types.MessageEntity] = None,
             disable_web_page_preview: bool = None,
             reply_markup: types.InlineKeyboardMarkup = None,
-            reply_to_message_id: int = None, timeout: int = None, message_thread_id: int = None
+            reply_to_message_id: int = None, timeout: int = None, message_thread_id: int = None,
+            delay_between_acting: float = 2
     ) -> list:
         to_return = []
         text_parts = util.smart_split(text=text)
@@ -46,13 +48,13 @@ class EncTeleBot(TeleBot):
             inline_message_id=inline_message_id, entities=entities, disable_web_page_preview=disable_web_page_preview,
             reply_markup=reply_markup
         )
-        time.sleep(1)
+        time.sleep(delay_between_acting)
         for t in text_parts:
             to_return.append(self.send_message(
                 chat_id=chat_id, text=t, parse_mode=parse_mode, disable_web_page_preview=disable_web_page_preview,
                 reply_to_message_id=reply_to_message_id, timeout=timeout, message_thread_id=message_thread_id
             ))
-            time.sleep(2)
+            time.sleep(delay_between_acting)
 
         return to_return
 
